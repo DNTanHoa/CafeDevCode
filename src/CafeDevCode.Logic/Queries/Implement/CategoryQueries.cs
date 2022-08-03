b545c712af216be3b0b3afeaeae59b21c166cb1e
Dương Nguyễn Tấn Hòa <tanhoatm@gmail.com>
2022-08-03 14:19:45 +0700

    Update code

--- src/CafeDevCode.Logic/Queries/Implement/CategoryQueries.cs
+++ src/CafeDevCode.Logic/Queries/Implement/CategoryQueries.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using CafeDevCode.Common.Shared.Model;
+using CafeDevCode.Database;
+using CafeDevCode.Logic.Queries.Interface;
+using CafeDevCode.Logic.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,101 @@ using System.Threading.Tasks;
 
 namespace CafeDevCode.Logic.Queries.Implement
 {
-    public class CategoryQueries
+    public class CategoryQueries : ICategoryQueries
     {
+        private readonly AppDatabase database;
+        private readonly IMapper mapper;
+
+        public CategoryQueries(AppDatabase database,
+            IMapper mapper)
+        {
+            this.database = database;
+            this.mapper = mapper;
+        }
+
+        public List<CategorySummaryModel> GetAll()
+        {
+            return database.Categories
+               .Select(x => mapper.Map<CategorySummaryModel>(x))
+               .ToList();
+        }
+
+        public Task<List<CategorySummaryModel>> GetAllAsync()
+        {
+            return Task.Run(() => database.Categories
+                .Select(x => mapper.Map<CategorySummaryModel>(x))
+                .ToListAsync());
+        }
+
+        public CategoryDetailModel? GetDetail(int id)
+        {
+            var category = database.Authors.FirstOrDefault(x => x.Id == id);
+
+            if (category != null)
+            {
+                var result = mapper.Map<CategoryDetailModel>(category);
+
+                var categoryPostIds = database.PostCategories.Where(x => x.CategoryId == id)
+                    .Select(x => x.PostId);
+
+                var posts = database.Post.Where(x => categoryPostIds.Contains(x.Id));
+
+                result.Posts = posts.ToList();
+                return result;
+            }
+
+            return null;
+        }
+
+        public Task<CategoryDetailModel?> GetDetailAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BasePagingData<CategorySummaryModel> GetPaging(BaseQuery query)
+        {
+            var categories = database.Categories
+                .Where(x => x.Title!.Contains(query.Keywords ?? string.Empty) ||
+                            x.Keywords!.Contains(query.Keywords ?? string.Empty) ||
+                            x.Description!.Contains(query.Keywords ?? string.Empty) ||
+                            x.IsDeleted != true)
+                .Skip(((query.PageIndex - 1) * query.PageSize) ?? 0).Take((query.PageSize * query.PageIndex) ?? 20)
+                .Select(x => mapper.Map<CategorySummaryModel>(x))
+                .ToList();
+
+            var authorCount = database.Authors.Count();
+
+            return new BasePagingData<CategorySummaryModel>()
+            {
+                Items = categories,
+                PageSize = query.PageSize ?? 1,
+                PageIndex = query.PageIndex ?? 20,
+                TotalItem = authorCount,
+                TotalPage = (int)Math.Ceiling((double)authorCount / (query.PageSize ?? 20))
+            };
+        }
+
+        public Task<BasePagingData<CategorySummaryModel>> GetPagingAsync(BaseQuery query)
+        {
+            var categories = database.Categories
+                .Where(x => x.Title!.Contains(query.Keywords ?? string.Empty) ||
+                            x.Keywords!.Contains(query.Keywords ?? string.Empty) ||
+                            x.Description!.Contains(query.Keywords ?? string.Empty) ||
+                            x.IsDeleted != true)
+                .Skip(((query.PageIndex - 1) * query.PageSize) ?? 0).Take((query.PageSize * query.PageIndex) ?? 20)
+                .Select(x => mapper.Map<CategorySummaryModel>(x))
+                .ToList();
+
+            var authorCount = database.Authors.Count();
+
+            return Task.FromResult(new BasePagingData<CategorySummaryModel>()
+            {
+                Items = categories,
+                PageSize = query.PageSize ?? 1,
+                PageIndex = query.PageIndex ?? 20,
+                TotalItem = authorCount,
+                TotalPage = (int)Math.Ceiling((double)authorCount / (query.PageSize ?? 20))
+            });
+        }
     }
 }
--- src/CafeDevCode.Logic/Queries/Interface/ICategoryQueries.cs
+++ src/CafeDevCode.Logic/Queries/Interface/ICategoryQueries.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CafeDevCode.Common.Shared.Model;
+using CafeDevCode.Logic.Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace CafeDevCode.Logic.Queries.Interface
 {
-    public interface ICategoriesQueries
+    public interface ICategoryQueries
     {
+        BasePagingData<CategorySummaryModel> GetPaging(BaseQuery query);
+        List<CategorySummaryModel> GetAll();
+        CategoryDetailModel? GetDetail(int id);
+        Task<CategoryDetailModel?> GetDetailAsync(int id);
+        Task<BasePagingData<CategorySummaryModel>> GetPagingAsync(BaseQuery query);
+        Task<List<CategorySummaryModel>> GetAllAsync();
     }
 }
