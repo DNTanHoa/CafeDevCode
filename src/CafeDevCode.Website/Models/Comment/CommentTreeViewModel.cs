f2c29cd332a5f6f0237f989983061118683dac60
Dương Nguyễn Tấn Hòa <tanhoatm@gmail.com>
2022-09-30 16:55:44 +0700

    update code

---
+++ src/CafeDevCode.Website/Models/Comment/CommentTreeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CafeDevCode.Logic.Shared.Models;
+using CafeDevCode.Ultils.Model;
+
+namespace CafeDevCode.Website.Models
+{
+    public class CommentTreeViewModel
+    {
+        public bool IsRoot { get; set; }
+        public IEnumerable<TreeList<CommentModel>> Collection { get; set; } 
+            = new List<TreeList<CommentModel>>();
+    }
+}
